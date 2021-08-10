c7de5c1785d632fd0b5d756584d1782c1ffc22d8
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2021-08-11 02:24:46 +0300

    feat: auth 4

--- app.js
+++ app.js
@@ -43,27 +43,27 @@ app.use(cookieParser());
 // app.options(cors(corsOptions));
 
 // Вариант 2
-const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'https://bitfilms.nomoredomains.monster',
-];
-app.options('*', (req, res) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
-});
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'http://localhost:3001',
+//   'https://bitfilms.nomoredomains.monster',
+// ];
+// app.options('*', (req, res) => {
+//   const { origin } = req.headers;
+//   if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(200);
+// });
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   if (allowedCors.includes(origin)) { res.header('Access-Control-Allow-Origin', origin); }
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 // *** Роуты ***
 app.use('/', require('./routes/index'));
